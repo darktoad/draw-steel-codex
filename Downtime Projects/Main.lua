bebfd97a8a0d0591e3bbf92e331e0456a7c8bfd2
Tim Clark <github@thc1967.com>
2026-01-28 23:03:53 -0500

    Fix nil dereference in downtime project follower roll migration

--- Downtime Projects/Main.lua
+++ Downtime Projects/Main.lua
@@ -91,7 +91,7 @@ local function _migrateFollowerRollsToHero()
                         for id,_ in pairs(followers) do
                             local follower = dmhub.GetCharacterById(id)
                             if follower and follower.properties and follower.properties:IsFollower() then
-                                local followerType = follower.try_get("followerType")
+                                local followerType = follower.typeName ~= nil and follower.try_get("followerType") or follower.followerType
                                 if followerType == "artisan" or followerType == "sage" then
                                     local legacyRolls = follower.properties:try_get(DTConstants.FOLLOWER_AVAILROLL_KEY, 0)
                                     migratedRolls[id] = legacyRolls
